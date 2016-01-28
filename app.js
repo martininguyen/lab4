806be256de6305e2fdfa65d3e85f81d027c93abf
Martin Nguyen <martinnguyen0618@gmail.com>
2016-01-28 12:27:46 -0800

    fixed a routing issue

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/project/martinspage', project.viewProject);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
