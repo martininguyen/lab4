bc904cc3bd1233911ef6785ba4797fef5422c281
Martin Nguyen <martinnguyen0618@gmail.com>
2016-01-28 12:46:45 -0800

    halkshgkld

--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,17 @@ exports.viewProject = function(req, res) {
   var name = req.params.name;
   console.log("The project name is: " + name);
   res.render('project', {
-    'projectName': name,
+    'projectName': name
+    'review' : [
+		{ 'occu': 'User',
+		  'desc': 'Salient need',		
+		},
+		{ 'occu': 'Schoolteacher',
+		  'desc': 'Wants to know what music students are listening to so he can integrate it into his lessons.'	,
+		},
+		{ 'occu': 'Student 5th grade',
+		  'desc': 'Desire to be seen as a tastemaker for her friends. Hears about new music from older sister in middle school.',
+		},
+	]
   });
 };
