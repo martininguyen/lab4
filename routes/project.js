aeeb02673583a111ee525129965b66245f91be30
Martin Nguyen <martinnguyen0618@gmail.com>
2016-01-28 12:47:31 -0800

    halkshgkld

--- routes/project.js
+++ routes/project.js
@@ -3,7 +3,7 @@ exports.viewProject = function(req, res) {
   var name = req.params.name;
   console.log("The project name is: " + name);
   res.render('project', {
-    'projectName': name
+    'projectName': name,
     'review' : [
 		{ 'occu': 'User',
 		  'desc': 'Salient need',		
